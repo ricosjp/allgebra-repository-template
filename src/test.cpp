166dc2bbf4687b1ccadbb70a1b4cb3ad9678bbf9
Toshiki Teramura <toshiki.teramura@gmail.com>
2021-09-20 07:08:10 +0900

    Add omp parallel for

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello Container!" << std::endl;
+    #pragma omp parallel for
+    for(size_t i = 0; i < 4; i++) {
+        std::cout << "Hello Container!" << std::endl;
+    }
     return 0;
 }
